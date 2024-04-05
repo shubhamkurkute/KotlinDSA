56c38b8b7cee89238904efd374881937470eca30
shubhcctech <shubham.kurkute@cctech.co.in>
2024-04-05 13:04:35 +0530

    Folder created for each DSA

--- src/main/kotlin/Main.kt
+++
@@ -1,5 +0,0 @@
-fun main(){
-    val smartDevices  :SmartDevices = SmartTVDevice("AndroidTV","Entertainment")
-    smartDevices.turnOn()
-
-}
--- src/main/kotlin/NumberOfOccurences.kt
+++
@@ -1,9 +0,0 @@
-fun main() {
-    var array = arrayOf(2,3,3,4,4,6,6,6,8)
-    var value = 3;
-    val firsIndex = findingOccurenceIndex(array,value)
-    val lastIndex = findingLastOccurenceIndex(array,value)
-
-    val occurrence = lastIndex-firsIndex + 1;
-    println(occurrence)
-}
